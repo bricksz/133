671c299045aec5b3adeebaf44579ffc470988f5f
bricksz <36253313+bricksz@users.noreply.github.com>
2018-09-06 14:48:26 -0800

    Add files via upload

--- Unit 4/3.py
+++ Unit 4/3.py
@@ -10,9 +10,63 @@ Rather than keeping all possible words in a single list, you should use a dictio
 the dictionary is called wordlists , then wordlists[7] should be a list of all words in
 Pride and Prejudice that are seven letters long.
 '''
-
 import random
 import functions as f
 
-list_words = f.word_arr()
 
+def cleanedup(s):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    cleantext = ''
+    for character in s.lower():
+        if character in alphabet:
+            cleantext += character
+        else:
+            cleantext += ' '
+    return cleantext
+    
+
+def max_word(list_words):
+    current = []
+    current_ix = 0
+    for word in list_words:
+        if len(word) > current_ix:
+            current_ix = len(word)
+            current = word
+        else:
+            pass
+    return current
+
+def unscramble_setup(words_in_array):
+    selected_word = random.choice(words_in_array)
+    scrambled_word = f.scramble(selected_word)
+
+    print("The word is: ", scrambled_word)
+    user_input = input("Please unscramble the word: ")
+    if user_input == selected_word:
+        print("You win")
+    else:
+        print("Try again")
+        
+
+
+wordlists = {}
+list_words = f.word_arr()
+max_ix = len(max_word(list_words))
+for i in range(max_ix):
+    wordlists.setdefault(i, [])
+    for j in list_words:
+        if i == len(j):
+            wordlists[i].append(j)
+            
+
+counter = 3
+while counter < 18:
+    select_word = random.choice(wordlists[counter])
+    scrambled_word = f.scramble(select_word)
+    print("The word is: ", scrambled_word)
+    user_input = input("Please unscramble the word: ")
+    if user_input == select_word:
+        print("You win")
+        counter += 1
+    else:
+        print("Try again")
--- Unit 4/test.py
+++ Unit 4/test.py
@@ -1,18 +1,6 @@
-import random
 
-def rejoin(word_arr):
-    text = ''
-    for letter in word_arr:
-        text += letter
-    return text
-
-def scramble(word):
-    emptylist = []
-    for i in range(len(word)):
-        emptylist.append(word[i])
-    random.shuffle(emptylist)
-    word = rejoin(emptylist)
-    return word
 
 word = 'cat'
-print(scramble(word))
+
+for i in word:
+    print(i)
